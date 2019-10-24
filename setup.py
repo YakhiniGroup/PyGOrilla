8efa39c5428d32c5c94bf7b509e0bdd254ecb269
MIDDLEEAST\shaybe <shaybe@microsoft.com>
2019-10-24 10:22:10 +0300

    Fixed setup to link to release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Shay Ben-Elazar',                   # Type in your name
   author_email = 'shaybenelazar@hotmail.com',      # Type in your E-Mail
   url = 'https://github.com/YakhiniGroup/PyGOrilla',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/YakhiniGroup/PyGOrilla/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['mHG', 'GOrilla', 'Gene Ontology'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'requests',
